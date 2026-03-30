a1d6167aa8d39e3f5b03ca99db11481723eea3a1
Mengf-git <2574418528@qq.com>
2026-03-30 11:10:44 +0800

    Update ablation_experiments.py

--- src/ablation_experiments.py
+++ src/ablation_experiments.py
@@ -30,7 +30,7 @@ rcParams['figure.titlesize'] = 16
 # =====================================================================
 # Key Configuration Parameters
 # =====================================================================
-FOLDER = "D:/Grade 1/GNSS-LSTM+Attention+SG/Spatial Correlation - Machine Learning/YNYS"
+FOLDER = "../data/real_GNSS"
 TARGET_STATION = "YNYS"
 NEIGHBOR_STATION = "YNLJ"
 TIME_COL = "YYYYMMDD"
